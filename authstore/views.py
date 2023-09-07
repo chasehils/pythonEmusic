5910d3626a1bef681a527521549d11bf79c60374
usernamechasehils <chasehilsinger@gmail.com>
2023-09-07 10:14:09 -0400

    static files working

--- authstore/views.py
+++ authstore/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signup(request):
-    
-    return render(request, "authentication/signup.html")
+    print("i am running sign up")
+    if (request.method =="POST"):
+        email=request.POST['email']
+        password=request.POST['pass1']
+        confirm_password=request.POST['pass2']
+        if password != confirm_password:
+            return HttpResponse('password is incorrect')
+            # return render(request, 'auth/signup.html')
+        
+        try:
+            if User.objects.get(username=email):
+                return HttpResponse('email is already in use')
+                # return render(request, 'auth/signup.html')
+
+        except Exception as identifier:
+            pass
+
+        user = User.objects.create_user(email, email, password)
+        user.save()
+    return render(request, "signup.html")
 
 def handlelogin(request):
     
-    return render(request, "authentication/login.html")
+    return render(request, "login.html")
 
 def handlelogout(request):
     
-    return redirect('/authstore/login')
+    return redirect('/auth/login')
